9ad7e51eee4bdbc63757eefc040ac608b6eb12aa
manas <manasmishra904@gmail.com>
2024-03-31 18:49:47 +0530

    signup

---
+++ Manas/Mindex.js
@@ -0,0 +1,9 @@
+let button=document.getElementById("button");
+
+let signInCard = document.getElementById("signInCard");
+
+button.addEventListener('click', function () {
+  signInCard.classList.toggle("hidden");
+});
+
+
